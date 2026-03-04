79539e14a18f3532a76edba1882f04355a239c01
Mika.el <lebris.mika@gmail.com>
2026-03-04 15:08:40 +0100

    add logger

--- alerts/src/main/java/io/safetynet/alerts/AlertsApplication.java
+++ alerts/src/main/java/io/safetynet/alerts/AlertsApplication.java
@@ -1,13 +1,16 @@
 package io.safetynet.alerts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlertsApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(AlertsApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(AlertsApplication.class, args);
+        SpringApplication.run(AlertsApplication.class, args);
+        logger.info("Application start");
 	}
 
 }
